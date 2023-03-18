7a4fd2452d725780c64883582fd1f91f8c6708f4
unknown <quocdza2@gmail.com>
2023-03-18 20:07:45 +0700

    commit note

--- baitapthuchanhlab2/src/bai9.java
+++ baitapthuchanhlab2/src/bai9.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
 public class bai9 {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
+        int solanxuathien = 0;
         String chuoi;
         char kytu;
-        Scanner sc = Scanner(System.in)
-        System.out.println("nhap vao chuoi bat ky :");
-        chuoi = Scanner.nextLine();
-        
-
-
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Nhap chuoi : ");
+            chuoi = sc.nextLine();
+        } while (chuoi.length() > 80);
+        System.out.println("Nhap ky tu can dem :");
+        kytu = sc.next().charAt(0);
+        for (int i = 0; i < chuoi.length(); i++) {
+            if (kytu == chuoi.charAt(i))
+                solanxuathien++;
+        }
+        System.out.println("So lan xuat hien ky tu " + kytu + " trong chuoi " + chuoi + "la : " + solanxuathien);
     }
 }
