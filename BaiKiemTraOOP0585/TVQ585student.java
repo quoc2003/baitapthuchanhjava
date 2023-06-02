96b93c3cf9c6b9083ff13336b861d7301b112435
unknown <quocdza2@gmail.com>
2023-06-02 10:03:52 +0700

    tvq505bai1

---
+++ BaiKiemTraOOP0585/TVQ585student.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.DimensionUIResource;
+
+public class TVQ585student {
+    public static void main(String[] args) {
+        String ten;
+        int tuoi;
+        float diem;
+        
+        
+    }
+    public static void nhapthongtin(){
+        Scanner sc = new Scanner(System.in);
+    System.out.println("nhap ho va ten :");
+    name= sc.nextLine();
+    System.out.println("nhap tuoi :");
+    age= sc.nextInt();
+    System.out.println("nhap ma sinh vien :");
+    diachi= sc.nextFloat();
+
+    }
+    public static Void themthongtin(){
+        System.out.println("nhap thong tin can them:");
+
+
+
+    }
+    public static Void suathongtin(){
+        System.out.println("nhap thong tin can sua :");
+
+    }
+    public static Void xoathongtin(){
+        System.out.println("nhap thong tin can xoa:");
+        sinhvien.remove;
+    }
+    public static Void hienthi(){
+        System.out.println("");
+
+    }
+
+
+    
+}
